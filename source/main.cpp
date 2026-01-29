bddce40af9c94680b34c85d5f931fd1dd873a084
taviken <trevor.viken@gmail.com>
2026-01-29 13:44:46 -0700

    updates

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,9 @@
 #define GLFW_INCLUDE_VULKAN
+
 // #include "source/glfw/include/glfw3.h"
-#include "glfw/include/GLFW/glfw3.h"
+// #include "glfw/include/GLFW/glfw3.h"
+#include <glfw/glfw3.h>
+
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/vec4.hpp>
