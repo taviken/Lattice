9ffc643e7e103a3d1d3548730f3dcdc785712987
taviken <trevor.viken@gmail.com>
2026-01-29 12:39:09 -0700

    updating git ignore

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,10 @@
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
+// #define GLFW_INCLUDE_VULKAN
+#include "glfw/include/GLFW/glfw3.h"
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
+#include "glm/glm/vec4.hpp"
+#include "glm/glm/mat4x4.hpp"
 
 #include <iostream>
 
