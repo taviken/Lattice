90f87ba91ff3a72655bf6b330e2c64e813bd1dd4
taviken <trevor.viken@gmail.com>
2026-01-29 15:20:06 -0700

    Added vkinstance

--- source/header/Window.h
+++ source/header/Window.h
@@ -4,6 +4,7 @@
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <stdexcept>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
@@ -15,10 +16,12 @@ public:
     void initVulkan();
     void mainLoop();
     void cleanup();
+    void createInstance();
 
 
 private:
     GLFWwindow* window;
+    VkInstance instance;
 
     
 };
--- source/src/Window.cpp
+++ source/src/Window.cpp
@@ -19,7 +19,7 @@ void Window::initWindow() {
     }
 
 void Window::initVulkan() {
-
+    createInstance();
 }
 
 void Window::mainLoop() {
@@ -32,4 +32,31 @@ void Window::cleanup() {
     glfwDestroyWindow(window);
 
     glfwTerminate();
+}
+
+void Window::createInstance() {
+    VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "Lattice";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        createInfo.enabledLayerCount = 0;
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
 }
